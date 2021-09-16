f15a1fc0d73a5d273b7b648c8e98cf46c6671163
mori08 <mrmt0814@gmail.com>
2021-09-16 15:17:50 +0900

    Add: Configグラスの追加

--- Ash/src/Main.cpp
+++ Ash/src/Main.cpp
@@ -1,8 +1,13 @@
 ﻿#include"MyLibrary/MyLibrary.hpp"
+#include"Config/Config.hpp"
 
 
 void Main()
 {
+	// ウィンドウの設定
+	Window::Resize(Ash::Config::get<Size>(U"Window.size"));
+	Window::SetTitle(Ash::Config::get<String>(U"Window.name"));
+
 	// アセット管理
 	Ash::registerAsset(U"asset/");
 
