8de2518784a611e544fbfafa934d61e5a11ed315
mori08 <mrmt0814@gmail.com>
2021-09-28 15:30:31 +0900

    Add: LoadSceneの追加

--- Ash/src/Scene/Load/LoadScene.hpp
+++ Ash/src/Scene/Load/LoadScene.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <future>
 #include "../Scene.hpp"
 
 namespace Ash
@@ -8,7 +9,37 @@ namespace Ash
 	{
 	private:
 
+		std::future<SceneName> m_loadThread;
 
+	public:
+
+		LoadScene(const InitData& init, std::function<SceneName()> loadFunc);
+
+		/// <remarks>
+		/// このコンストラクタを使用する場合
+		/// 必ずsetLoadThreadで関数を設定すること
+		/// </remarks>
+		LoadScene(const InitData& init);
+
+		virtual ~LoadScene();
+
+	private:
+
+		void update() override;
+
+		virtual void draw() const override;
+
+		void drawFadeIn(double) const override;
+
+		void drawFadeOut(double) const override;
+
+	protected:
+
+		/// <summary>
+		/// スレッドに関数の設定
+		/// </summary>
+		/// <param name="loadFunc"> 関数 </param>
+		void setLoadThread(std::function<SceneName()> loadFunc);
 
 	};
 }
